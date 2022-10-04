66ef210e3af203b02d474752b29fb1525953ae40
Wandole <mchirez@gmail.com>
2022-10-04 21:55:52 +0200

    Error fix: Add title to mocked datas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,10 +11,10 @@ function Home() {
       <div className="hero">
         <section className="hero-content">
           <h2 className="sr-only">Promoted Content</h2>
-          <p className="subtitle">No fees.</p>
-          <p className="subtitle">No minimum deposit.</p>
-          <p className="subtitle">High interest rates.</p>
-          <p className="text">Open a savings account with Argent Bank today!</p>
+          <p className="subtitle">{text.hero.titleA}</p>
+          <p className="subtitle">{text.hero.titleB}</p>
+          <p className="subtitle">{text.hero.titleC}</p>
+          <p className="text">{text.hero.text}</p>
         </section>
       </div>
       <section className="features">
